50befff482ff9fb344f911d504b86284743b82fe
Jakub Kosior <jakubjkosior@gmail.com>
2018-03-23 19:06:06 +0100

    get request permissions

--- app/src/main/java/com/example/jakubkosior/memegenerator/MainActivity.java
+++ app/src/main/java/com/example/jakubkosior/memegenerator/MainActivity.java
@@ -1,15 +1,23 @@
 package com.example.jakubkosior.memegenerator;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final int REQUEST_CODE = 10;
+    public static final int REQUEST_CODE_STORAGE = 20;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +26,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void pickPhotoFromGallery(View v){
+
+
+    }
+
+    private void photoIntentCreator() {
         Intent photoIntent = new Intent(Intent.ACTION_PICK);
 
         File photoDirectory = Environment
@@ -34,6 +47,49 @@ public class MainActivity extends AppCompatActivity {
 
         photoIntent.setDataAndType(photoUri,"image/*");
 
-        startActivityForResult(photoIntent,);
+        startActivityForResult(photoIntent, REQUEST_CODE);
+    }
+
+    private void requestPermission(){
+        if(ContextCompat.checkSelfPermission(
+                this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED){
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                    REQUEST_CODE_STORAGE);
+
+        }
+    }
+
+    @Override
+    private void onRequestPermissionResult(int requestCode,
+                                           String[] permissions,
+                                           int[] grantResults){
+
+        switch(requestCode){
+
+            case REQUEST_CODE_STORAGE: {
+                if(grantResults.length > 0 &&
+                        grantResults[0] == PackageManager.PERMISSION_GRANTED){
+
+                    /**
+                     * If permission granted, get image
+                     */
+
+                    this.photoIntentCreator();
+
+                } else {
+
+                    /**
+                     * If permission denied, show toast
+                     */
+
+                    Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        }
+
     }
 }
